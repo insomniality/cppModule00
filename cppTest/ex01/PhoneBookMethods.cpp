9460c4615ecdf0dfd57996cec99e3c5c0cd94c38
Mikayel Arzumanyan <mikarzum@c2r3s2.42yerevan.am>
2023-07-23 21:23:06 +0400

    2

--- cppTest/ex01/PhoneBookMethods.cpp
+++ cppTest/ex01/PhoneBookMethods.cpp
@@ -1,9 +1,48 @@
+#include <iostream>
 #include "PhoneBook.hpp"
 
 PhoneBook::PhoneBook()
 {
 	i = 0;
 	for (int i = 0; i < 8; i++)
-		contacts[i].Index = -1;
+		contacts[i].setIndex(-1);
+}
+
+void PhoneBook::add()
+{
+	std::string buff;
+
+	contacts[i].setIndex(i);
+	std::cout << "Enter your First Name : ";
+	std::cin >> buff;
+	contacts[i].setString(buff, "FN");
+	std::cout << "Enter your Last Name : ";
+	std::cin >> buff;
+	contacts[i].setString(buff, "LN");
+	std::cout << "Enter your Nick Name : ";
+	std::cin >> buff;
+	contacts[i].setString(buff, "NN");
+	std::cout << "Enter your Phone Number : ";
+	std::cin >> buff;
+	contacts[i].setString(buff, "PN");
+	std::cout << "Enter your Darkest Secret : ";
+	std::cin >> buff;
+	contacts[i].setString(buff, "DS");
+	i++;
+}
+
+void PhoneBook::search()
+{
+
+}
+
+void PhoneBook::searchPrint(int a, int b)
+{
+
+}
+
+void PhoneBook::exit()
+{
+	std::exit(1);
 }
 
